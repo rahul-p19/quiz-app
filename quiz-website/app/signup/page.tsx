a4754b0d5e43fcbb66d81bf5ddad24feae5f1a66
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2024-12-30 01:56:38 +0530

    signup ui changed

--- quiz-website/app/signup/page.tsx
+++ quiz-website/app/signup/page.tsx
@@ -2,17 +2,19 @@ import SignupForm from "./clientCode";
 import React from "react";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
 
 async function Signup() {
   const session = await auth();
   if(session && session.user.role=='ADMIN') redirect("/admin");
   if(session && session.user.role=='USER') redirect("/");
   // console.log(session);
-	return <main className="flex flex-col justify-center items-center h-full">
+	return (<><main className="flex flex-col justify-center items-center h-full">
     <div className="bg-gradient-to-tr from-[#00000040] via-[#00000020] to-secondary backdrop-blur-lg border border-[#ffffff40] rounded-xl shadow-lg p-6 mx-2 max-w-sm flex flex-col items-center gap-y-4">
       <SignupForm />
     </div>
-  </main>;
+  </main><ShootingStars/><StarsBackground/></>);
 }
 
 export default Signup;
