8098250395d1d2c75be37daed202049b40e24969
Akash09 <akashbag12a05@gmail.com>
2025-01-01 00:25:49 +0530

    postinstall script added

--- quiz-website/components/Navigation/Navbar/index.tsx
+++ quiz-website/components/Navigation/Navbar/index.tsx
@@ -5,7 +5,7 @@ import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
 import { FaMapLocationDot } from "react-icons/fa6";
 import { FcAbout } from "react-icons/fc";
 import { RiTimeLine } from "react-icons/ri";
-import {IoPerson} from "react-icons/io5";
+import { IoPerson } from "react-icons/io5";
 export function Navbar() {
   const navItems = [
     {
@@ -21,28 +21,23 @@ export function Navbar() {
     {
       name: "Timelines",
       sectionId: "timelines",
+      icon: <RiTimeLine className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Venue",
+      sectionId: "venue",
       icon: (
-        <RiTimeLine className="h-4 w-4 text-neutral-500 dark:text-white" />
+        <FaMapLocationDot className="h-4 w-4 text-neutral-500 dark:text-white" />
       ),
     },
-      {
-        name: "Venue",
-        sectionId: "venue",
-        icon: (
-          <FaMapLocationDot className="h-4 w-4 text-neutral-500 dark:text-white" />
-        ),
-      },
-      {
-        name: "Speakers",
-        sectionId: "speakers",
-        icon: (
-          <IoPerson className="h-4 w-4 text-neutral-500 dark:text-white" />
-        ),
-      },
-      
+    {
+      name: "Speakers",
+      sectionId: "speakers",
+      icon: <IoPerson className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
   ];
   return (
-    <div className="relative  w-full">
+    <div className="relative  w-full pt-4">
       <FloatingNav navItems={navItems} />
     </div>
   );
--- quiz-website/components/ui/floating-navbar.tsx
+++ quiz-website/components/ui/floating-navbar.tsx
@@ -62,33 +62,36 @@ export const FloatingNav = ({
           y: isHomePage ? 0 : -100,
         }}
         animate={{
-          y: isHomePage ? 0 : (visible ? 0 : -100),
-          opacity: isHomePage ? 1 : (visible ? 1 : 0),
+          y: isHomePage ? 0 : visible ? 0 : -100,
+          opacity: isHomePage ? 1 : visible ? 1 : 0,
         }}
         transition={{
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  top-0 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit  top-8 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
           className
         )}
       >
         {navItems.map((navItem: any, idx: number) => (
           <button
-          key={`nav-item-${idx}`}
-          onClick={() => scrollToSection(navItem.sectionId)}
-          className={cn(
-            "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-          )}
-        >
-          <span className="block sm:hidden">{navItem.icon}</span>
-          <span className="hidden sm:block text-sm">{navItem.name}</span>
-        </button>
+            key={`nav-item-${idx}`}
+            onClick={() => scrollToSection(navItem.sectionId)}
+            className={cn(
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden sm:block text-sm">{navItem.name}</span>
+          </button>
         ))}
-         <button onClick={() => scrollToSection("quiz")} className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        <button
+          onClick={() => scrollToSection("quiz")}
+          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"
+        >
           <span>∞</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
-        </button> 
+        </button>
       </motion.div>
     </AnimatePresence>
   );
