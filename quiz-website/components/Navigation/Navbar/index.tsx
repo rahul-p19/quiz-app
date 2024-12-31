d72a39536d9d732c98c25adfb1282eb89b714bee
charliehustlr1792 <nilangshubhattacharyya1234@gmail.com>
2024-12-31 22:19:11 +0530

    navicons

--- quiz-website/components/Navigation/Navbar/index.tsx
+++ quiz-website/components/Navigation/Navbar/index.tsx
@@ -2,6 +2,10 @@
 import React from "react";
 import { FloatingNav } from "../../ui/floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { FcAbout } from "react-icons/fc";
+import { RiTimeLine } from "react-icons/ri";
+import {IoPerson} from "react-icons/io5";
 export function Navbar() {
   const navItems = [
     {
@@ -12,27 +16,27 @@ export function Navbar() {
     {
       name: "About",
       sectionId: "about",
-      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <FcAbout className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Timelines",
       sectionId: "timelines",
       icon: (
-        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+        <RiTimeLine className="h-4 w-4 text-neutral-500 dark:text-white" />
       ),
     },
       {
         name: "Venue",
         sectionId: "venue",
         icon: (
-          <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+          <FaMapLocationDot className="h-4 w-4 text-neutral-500 dark:text-white" />
         ),
       },
       {
         name: "Speakers",
         sectionId: "speakers",
         icon: (
-          <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+          <IoPerson className="h-4 w-4 text-neutral-500 dark:text-white" />
         ),
       },
       
