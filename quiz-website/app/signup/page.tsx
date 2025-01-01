bd00f0e2ec7af05aee2ffac9cee4249056af0b98
Akash09 <akashbag12a05@gmail.com>
2025-01-01 17:20:27 +0530

    ui changes continues

--- quiz-website/app/signup/page.tsx
+++ quiz-website/app/signup/page.tsx
@@ -13,15 +13,19 @@ async function Signup() {
     <main className="flex flex-row justify-center items-center w-full h-screen">
       <div className="w-1/2 h-full bg-white hidden md:block">
         <BackgroundBeamsWithCollision>
-          <h2 className="text-2xl relative z-20 md:text-5xl font-bold text-center text-white tracking-tight">
+          <h2 className="text-2xl relative px-8 z-20 md:text-5xl font-bold text-center text-white tracking-tight">
             {/* What&apos;s cooler than Beams?{" "} */}
-            How to solve Engineering ?{" "}
+            From dark pits of oblivion to the hallows of glory,
             <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
               <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-                <span className="">Hello-IEEE.</span>
+                <span className="">
+                  Begin this galactic <br /> event with a bang !
+                </span>
               </div>
               <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-                <span className="">Hello-IEEE.</span>
+                <span className="">
+                  Begin this galactic <br /> event with a bang !
+                </span>
               </div>
             </div>
           </h2>
