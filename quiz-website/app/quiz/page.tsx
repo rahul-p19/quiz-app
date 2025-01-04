e7c0a470b383feefcde7a31a26678f92b0967569
rahul-p19 <pandeyrahul.work@gmail.com>
2025-01-04 14:30:24 +0000

    linting fix

--- quiz-website/app/quiz/page.tsx
+++ quiz-website/app/quiz/page.tsx
@@ -2,7 +2,6 @@ import { auth } from '@/auth'
 import ClientCode from '@/app/quiz/ClientCode';
 import { redirect } from 'next/navigation';
 import React from 'react'
-import { getAllQuestions } from './getQuestions';
 
 async function QuizPage() {
   const session = await auth();
