9371e141bdc0811865b236f2774cc55b8b88d3c8
rahul-p19 <pandeyrahul.work@gmail.com>
2025-06-28 18:33:26 +0530

    feat: add singleton kafka client

--- quiz-website/lib/kafka.ts
+++ quiz-website/lib/kafka.ts
@@ -1,18 +1,30 @@
-import {Kafka} from "kafkajs";
+import {Kafka, Producer} from "kafkajs";
 
-const KAFKA_CERT = Buffer.from(process.env.KAFKA_CA_CERT??"", 'base64').toString('utf-8')
+const getKafkaProducer = () => {
+    const KAFKA_CERT = Buffer.from(process.env.KAFKA_CA_CERT??"", 'base64').toString('utf-8')
+    console.log(KAFKA_CERT);
+    const kafka = new Kafka({
+        clientId: "quiz-app",
+        brokers: [process.env.NEXT_PUBLIC_KAFKA_URL??""],
+        ssl:{
+            ca: KAFKA_CERT
+        },
+        sasl: {
+            username: "avnadmin",
+            password: process.env.KAFKA_PASSWORD!,
+            mechanism: "plain"
+        }
+    })
+    
+    const kafkaProducer = kafka.producer();
+    return kafkaProducer;
+}
 
-const kafka = new Kafka({
-    clientId: "quiz-app",
-    brokers: [process.env.NEXT_PUBLIC_KAFKA_URL??""],
-    ssl:{
-        ca: KAFKA_CERT
-    },
-    sasl: {
-        username: "avnadmin",
-        password: process.env.KAFKA_PASSWORD!,
-        mechanism: "plain"
-    }
-})
+declare global {
+    // eslint-disable-next-line no-var
+    var kafkaProducer: Producer | undefined;
+}
 
-export const producer = kafka.producer();
+export const producer = globalThis.kafkaProducer || getKafkaProducer();
+
+if(process.env.NODE_ENV !== "production") globalThis.kafkaProducer = producer;
