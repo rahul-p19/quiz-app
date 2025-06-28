5b9704411d0daecbe06ee4d9b9ff20f3c536f0d9
rahul-p19 <pandeyrahul.work@gmail.com>
2025-06-28 18:24:21 +0530

    fix: env variables

--- quiz-website/lib/kafka.ts
+++ quiz-website/lib/kafka.ts
@@ -4,9 +4,9 @@ const KAFKA_CERT = Buffer.from(process.env.KAFKA_CA_CERT??"", 'base64').toString
 
 const kafka = new Kafka({
     clientId: "quiz-app",
-    brokers: [KAFKA_CERT],
+    brokers: [process.env.NEXT_PUBLIC_KAFKA_URL??""],
     ssl:{
-        ca: process.env.KAFKA_CA_CERT
+        ca: KAFKA_CERT
     },
     sasl: {
         username: "avnadmin",
